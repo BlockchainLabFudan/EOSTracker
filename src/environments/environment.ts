af442bf691a972fa00612a17b244cbf635239e03
sdumaoziqi <15866605371@163.com>
2019-03-27 16:51:44 +0800

    symbol  EOS -> GOC

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,6 @@ export const environment = {
   votingUrl: 'https://eosportal.io',
   appName: 'GOC Tracker',
   logoUrl: '/assets/logo.png',
-  blockchainUrl: 'http://118.190.154.168:8000',
+  blockchainUrl: 'http://api.goclab.io:8080',
   chainId: '1cf6b75731df97242f6a5bc2f2adcefacc279ff72986c3352cb51a2f9e2df3ef'
 };
