307be8f80c6f3ccb9703a7c2347e595d2811e4df
sdumaoziqi <15866605371@163.com>
2019-05-13 21:13:50 +0800

    update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,14 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
 export const environment = {
   production: true,
   walletUrl: 'https://walleteos.com',
   votingUrl: 'https://eosportal.io',
   appName: 'GOC Tracker',
   logoUrl: '/assets/logo.png',
-  blockchainUrl: 'http://118.190.154.168:8000',
+  blockchainUrl: 'http://api.goclab.io:8080',
   chainId: '1cf6b75731df97242f6a5bc2f2adcefacc279ff72986c3352cb51a2f9e2df3ef'
 };
